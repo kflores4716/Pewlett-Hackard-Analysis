ef0abba25562a08ed9e5bd55b16c3c125a2f0142
kflores4716 <kflores4716@gmail.com>
2022-02-02 10:07:51 -0700

    Adding queries to repo

--- schema.sql
+++ schema.sql
@@ -16,11 +16,20 @@ CREATE TABLE employees (
 	PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE dept_employees (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
 CREATE TABLE dept_manager (
-	dept_no VARCHAR (4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
-	to_dat DATE NOT NULL,
+	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
@@ -35,23 +44,17 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_employees (
-	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
-);
-
 CREATE TABLE titles (
-	emp_no INT NOT NULL,
+	emp_no INT,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-SELECT * FROM departments;
+
+
+SELECT * FROM titles;
+
+DROP TABLE titles CASCADE;
 
