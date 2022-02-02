37e282f73e6194753e85e468f9c689507087b562
kflores4716 <kflores4716@gmail.com>
2022-02-02 14:39:43 -0700

    Adding dept-specific tables

--- queries.sql
+++ queries.sql
@@ -37,3 +37,144 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- View table to make sure it was created properly
 SELECT * FROM retirement_info;
+
+-- Drop the retirement_info table
+DROP TABLE retirement_info;
+
+-- Create new table for Retiring Employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the Table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+-- Came back to update table names using Aliases
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_employees.to_date
+FROM retirement_info
+LEFT JOIN dept_employees
+ON retirement_info.emp_no = dept_employees.emp_no;
+
+-- Recreating the above list using Aliases
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info AS ri
+LEFT JOIN dept_employees AS de
+ON ri.emp_no = de.emp_no;
+
+-- Creating a table from the above list of  employees
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_employees AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Check to make sure table was created
+SELECT * FROM current_emp;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_count_by_dept
+FROM current_emp AS ce
+LEFT JOIN dept_employees AS de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Check table to make sure its correct
+SELECT * FROM emp_count_by_dept;
+
+-- Check salaries table for to_date
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- to_date on salaries table isn't what we're looking for
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees AS e
+INNER JOIN salaries AS s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_employees AS de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+SELECT * FROM emp_info;
+
+-- List of managers per department
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp AS ce
+		ON (dm.emp_no = ce.emp_no);
+
+
+-- Creating a department info table
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_employees AS de
+	ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+	ON (de.dept_no = d.dept_no);
+
+-- Creating list from retirement_info only for Sales Team
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	d.dept_name
+INTO retirement_info_dept_name
+FROM retirement_info as ri
+INNER JOIN dept_employees AS de
+	ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+	ON (de.dept_no = d.dept_no);
+
+SELECT * 
+INTO retirement_info_sales
+FROM retirement_info_dept_name
+WHERE dept_name IN ('Sales');
+
+SELECT * FROM retirement_info_sales_development;
+
+SELECT * 
+INTO retirement_info_sales_development
+FROM retirement_info_dept_name
+WHERE dept_name IN ('Sales', 'Development');
